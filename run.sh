dd9b1850a9ac644d5fb3d8c5ae5174d93b06b2ac
Moh. Ilham Ramadan <mohilhamramadana721@gmail.com>
2025-02-03 14:05:54 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Starting License Plate Recognition System..."
+source Folder pengguna: {user_folder}/venv/bin/activate  
+cd Folder pengguna: {user_folder}
+python3 src/webcam_stream.py
